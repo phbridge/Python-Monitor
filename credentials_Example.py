af9a37418d1fe0035cac36b4c37dbca0ca759543
chrismagson <cmagson@cisco.com>
2020-07-29 13:45:06 +0100

    Add files via upload

--- credentials_Example.py
+++ credentials_Example.py
@@ -1,7 +1,8 @@
-ABSOLUTE_PATH = "/Users/phbridge/Documents/Git/Python-Monitor/"
-FLASK_HOST = "::"
+ABSOLUTE_PATH = "/app/"
+FLASK_HOST = "127.0.0.1"
 FLASK_PORT = 8050
 LOGFILE = ABSOLUTE_PATH + "/Python-Monitor_%s.log" % FLASK_PORT
 LOGCOUNT = 16
 LOGBYTES = 16777216
 INTERFACE = "ens192"
+INFLUX_DB_Path = "http://<hostname>:<port>/write?db=<DBNAME>&u=<USERNAME>&p=<password>"
